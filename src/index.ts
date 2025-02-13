abcf0a022aad61a1c98fbcc04bb2fd118f7ad1ae
Ben Halverson <benhalverson33@gmail.com>
2025-02-13 12:36:09 -0800

    cors and logging

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { drizzle, } from "drizzle-orm/d1";
 import { eq, sql, } from "drizzle-orm";
+import { z } from "zod";
 
 
 import { comments, posts } from "./db/schema";
@@ -8,23 +9,27 @@ import { create } from './controllers/create';
 import { list } from './controllers/list';
 import { deleteStuff } from './controllers/delete';
 import { update } from './controllers/update';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
 
 const app = new Hono<{ Bindings: Env }>();
 
+app.use(cors({ origin: '*' }));
+app.use(logger());
 app.get("/list", list);
 
 app.get('/list-comments', async (c) => {
   const db = drizzle(c.env.DB);
-  const response = await db.select({posts: posts.id}).from(posts).all();
+  // const response = await db.select({posts: posts.id}).from(posts).all();
 
-  // const response = db.select({
-  //   postId: posts.id,
-  //   title: posts.title,
-  //   commentCount: sql`COUNT(${comments.id}).as("commentCount")`,
-  // }).from(posts)
-  // .leftJoin(comments, eq(posts.id, comments.postId))
-  // .groupBy(posts.id)
-  // .all();
+  const response = db.select({
+    postId: posts.id,
+    title: posts.title,
+    commentCount: sql`COUNT(${comments.id}).as("commentCount")`,
+  }).from(posts)
+  .leftJoin(comments, eq(posts.id, comments.postId))
+  .groupBy(posts.id)
+  .all();
   return c.json(response);
 });
 
@@ -33,11 +38,16 @@ app.post("/create", create);
 app.post('/create-comment', async (c) => {
   const db = drizzle(c.env.DB);
   const { post_id, content } = await c.req.json();
-  const response = await db
+  try {
+    const response = await db
     .insert(comments)
     .values({ postId: post_id, content })
-    .returning();
-  return c.json(response);
+    .returning(); 
+    console.log('response', response);
+    return c.json(response);   
+  } catch (error: any) {
+    return c.json({error: error.message});
+  }
 });
 
 app.put("/update", update);
